21c48d8f0530f8fdcf1a26e304b635c5a35f2df2
mokbhai <mokshitjain18@gmail.com>
2024-10-21 11:21:40 +0530

    drop reg

--- DropRegistration.sql
+++ DropRegistration.sql
@@ -7,3 +7,7 @@ GO
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Registrations]') AND type in (N'U'))
 DROP TABLE [dbo].[Registrations]
 GO
+
+-- IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Payments]') AND type in (N'U'))
+-- DROP TABLE [dbo].[Payments]
+-- GO
