5c52861d6b32f7b75acc623026239c7a1607e454
mokbhai <mokshitjain18@gmail.com>
2024-10-29 11:03:05 +0530

    drop registrations

--- DropRegistration.sql
+++ DropRegistration.sql
@@ -4,6 +4,9 @@ GO
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TeamMembers]') AND type in (N'U'))
 DROP TABLE [dbo].[TeamMembers]
 GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Payments]') AND type in (N'U'))
+DROP TABLE [dbo].[Payments]
+GO
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Registrations]') AND type in (N'U'))
 DROP TABLE [dbo].[Registrations]
 GO
